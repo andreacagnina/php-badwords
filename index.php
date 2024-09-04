879d6bcf334146a1f3b242573b99829b6008de77
andreacagnina <amirkal@libero.it>
2024-09-04 15:11:41 +0200

    [feat] add form

--- index.php
+++ index.php
@@ -7,9 +7,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Badwords</title>
 </head>
 <body>
-    
+    <header>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-3">
+                    <div class="content">
+                        <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Inserisci una parola da censurare</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="***">
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <div class="content">
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Scrivi un testo</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" cols="50"></textarea>
+                        </div>
+                            <div class="col-12">
+                            <button class="btn btn-primary" type="submit">Invia</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+    </header>
 </body>
 </html>
